6707fc83caf202c6cb816208b8d4961274820fc5
Miigon <miigon.pg@gmail.com>
2023-05-03 08:57:29 +0800

    fix vscode theme require error in vscode-neovim

--- myconf.lua
+++ myconf.lua
@@ -2,14 +2,18 @@
 vim.o.background = 'dark'
 
 
-local c = require('vscode.colors').get_colors()
-require('vscode').setup({
-    transparent = true,
-    italic_comments = true,
-    disable_nvimtree_bg = true,
+local isVsCode = require('vscode').setup == nil -- otherwise require vscode will return the theme
+if not isVsCode then
+    local c = require('vscode.colors').get_colors()
 
-    color_overrides = {
-        -- vscLineNumber = '#FFFFFF',
-    },
-})
-require('vscode').load()
+    require('vscode').setup({
+        transparent = true,
+        italic_comments = true,
+        disable_nvimtree_bg = true,
+
+        color_overrides = {
+            -- vscLineNumber = '#FFFFFF',
+        },
+    })
+    require('vscode').load()
+end
